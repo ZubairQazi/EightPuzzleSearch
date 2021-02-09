6c845b9de16288615e6229fb8ff45bf2cb52b32b
Zubair Qazi <zubair.qazi.5b@gmail.com>
2021-02-08 19:54:23 -0800

    Implement general search algorithm

--- eightpuzzle.py
+++ eightpuzzle.py
@@ -1,5 +1,36 @@
 from node import Node
 
+goal = [[1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 0]]
 
-def search():
-    return 0
+
+def main():
+    puzzle = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 0, 8]]
+
+    root = Node(puzzle, 0, 0, [])
+    search(root, 0)
+
+
+# general search function
+def search(root, heuristic):
+    nodes = [root]
+
+    while len(nodes) != 0:
+        node = nodes.pop(0)
+        if node == goal:
+            return node
+        nodes = update_queue(nodes, heuristic)
+
+    print('No solution found')
+    return -1
+
+
+def update_queue(nodes, heuristic):
+    # TODO: Implement queueing function
+    return []
+
+
+main()
